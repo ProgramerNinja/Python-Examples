25abbd829b45f8fcd0fe0e29597f9c7c911c8db1
Jared McMahon <jared.mcmahon@tooeleschools.org>
2018-09-25 14:10:50 -0600

    updating clock program

--- time project.py
+++ time project.py
@@ -3,6 +3,10 @@
 
 import time
 import calendar
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+import winsound
 
 def clock():
     total_seconds = calendar.timegm(time.gmtime())
@@ -18,14 +22,35 @@ def clock():
 
     current_hour = total_hours % 60
 
+    current_hour = current_hour % 24
+
     current_hour = current_hour - 6
 
 
     print(current_hour , ":" , current_minute , ":" , current_second)
+    
 
-clock()
+    if current_hour <=12:
+        tag="PM"
+    else:
+        tag="AM"
+    timex = str(current_hour) + ":" + str(current_minute) + ":" + str(current_second) + tag
+    return timex
 
-i=1
-while i==1:
-    clock()
-    time.sleep(1)
+def quit(*args):
+    root.destroy()
+def show_time():
+    time = clock()
+    txt.set(time)
+    root.after(1000, show_time)
+
+root = Tk()
+root.attributes("-fullscreen", True)
+root.configure(background='teal')
+root.bind("x", quit)
+root.after(1000, show_time)
+fnt=font.Font(family='Pixel', size=60, weight='bold')
+txt=StringVar()
+lbl=ttk.Label(root, textvariable=txt, font=txt, foreground='blue', background="teal")
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+root.mainloop()
